7846ad2f3f82896859e47ea892e6dfb87403d088
Dylan GIL AMARO <dylan.flame.libra@gmail.com>
2024-03-11 13:56:32 +0100

    Revert ":tada: started project with pwa"
    This reverts commit 818ca65148ff3ae1338c3383167f099cdfc393b4.

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
-import withPWA from "@ducanh2912/next-pwa";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-  // ... other options you like
-};
-
-const withPWAConfig = withPWA({
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  dest: "public",
-  fallbacks: {
-    //image: "/static/images/fallback.png",
-    document: "/offline", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  // ... other options you like
-});
-
-export default withPWAConfig(nextConfig);
+export default nextConfig;
